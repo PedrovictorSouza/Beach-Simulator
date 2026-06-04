3e81f8f526d6afbd581171b2e9a5a276312267ec
PEDRO VICTOR <48770714+PedrovictorSouza@users.noreply.github.com>
2026-06-03 22:09:57 -0300

    Integrate rustling grass particle helper

--- app/runtime/gameLoop.js
+++ app/runtime/gameLoop.js
@@ -26,6 +26,7 @@ import { createRepairBoxMotionRuntime } from "./repairBoxMotionRuntime.js";
 import { createRepairBoxRevealFlashRuntime } from "./repairBoxRevealFlashRuntime.js";
 import { getRepairBoxRevealRayBillboards } from "./repairBoxRevealRayBillboards.js";
 import { createRunBreadcrumbPromptRuntime } from "./runBreadcrumbPromptRuntime.js";
+import { getRustlingGrassParticleBillboards } from "./rustlingGrassParticleBillboards.js";
 import { getSavePointStarBillboards } from "./savePointStarBillboards.js";
 import { createSnowstormFogRuntime } from "./snowstormFogRuntime.js";
 import { createTreeRevivalLeafBurstRuntime } from "./treeRevivalLeafBurstRuntime.js";
@@ -9940,32 +9941,6 @@ export function startGameLoop({
         uvRect,
         alpha: fadeIn * fadeOut,
         rotation: particle.rotation + floatWobble
-      };
-    });
-  }
-
-  function getRustlingGrassParticleBillboards(groundGrassPatch, texture, now, uvRect) {
-    if (!texture) {
-      return [];
-    }
-
-    const time = now * 0.001;
-
-    return [0, 1, 2, 3, 4].map((index) => {
-      const cycle = (time * 0.58 + index * 0.19) % 1;
-      const angle = time * 1.7 + index * 1.38;
-      const radius = 0.18 + (index % 3) * 0.08;
-      const size = (0.13 + (index % 2) * 0.025) * (1 - cycle * 0.28);
-
-      return {
-        texture,
-        position: [
-          groundGrassPatch.position[0] + Math.cos(angle) * radius,
-          groundGrassPatch.position[1] + 0.38 + cycle * 0.58,
-          groundGrassPatch.position[2] + Math.sin(angle) * radius
-        ],
-        size: [size, size],
-        uvRect
       };
     });
   }
