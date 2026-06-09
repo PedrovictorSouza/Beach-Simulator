413bc3eac93853925f258a758b7573765721b8f7
PEDRO VICTOR <48770714+PedrovictorSouza@users.noreply.github.com>
2026-06-09 13:03:19 -0300

    Remove AI workflow prompt button from app boot

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 import { createApplicationRuntime } from "./app/bootstrap/createApplicationRuntime.js";
-import { installAiWorkflowPromptTool } from "./app/ui/aiWorkflowPromptTool.js";
 
 const app = createApplicationRuntime();
-
-installAiWorkflowPromptTool();
 
 app.start();
